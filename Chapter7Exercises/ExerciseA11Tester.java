4162ccd59f0d39cb439deb98f9e4180f6009e6bd
davidkeefe10 <55868115+davidkeefe10@users.noreply.github.com>
2019-11-27 18:10:59 -0500

    Update and rename Exercise11Tester.java to ExerciseA11Tester.java

--- Chapter7Exercises/ExerciseA11Tester.java
+++ Chapter7Exercises/ExerciseA11Tester.java
@@ -1,11 +1,11 @@
 /**
  * This class tests the Exercise11 class.
  */
-public class Exercise11Tester
+public class ExerciseA11Tester
 {
     public static void main(String[] args)
     {
-        Exercise11 test = new Exercise11();
+        ExerciseA11 test = new ExerciseA11();
         System.out.println(test.isPerfectSquare(16));
     }
 }
